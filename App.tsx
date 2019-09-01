388b6f8f439829b72e48c4890e804e974a10b79c
Ryan Magoon <mr.magoon5@gmail.com>
2019-08-31 22:36:46 -0400

    add auth and firebase linking info

--- App.tsx
+++ App.tsx
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import { Platform, StyleSheet, Text, View } from 'react-native'
 
 import firebase from '@react-native-firebase/app'
+import '@react-native-firebase/auth'
 
 // TODO(you): import any additional firebase services that you require for your app, e.g for auth:
 //    1) install the npm package: `yarn add @react-native-firebase/auth@alpha` - you do not need to
@@ -10,20 +11,7 @@ import firebase from '@react-native-firebase/app'
 //    3) import the package here in your JavaScript code: `import '@react-native-firebase/auth';`
 //    4) The Firebase Auth service is now available to use here: `firebase.auth().currentUser`
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\nCmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\nShake or press menu button for dev menu'
-})
-
-const firebaseCredentials = Platform.select({
-  ios: 'https://invertase.link/firebase-ios',
-  android: 'https://invertase.link/firebase-android'
-})
-
-type Props = {}
-
-export default class App extends Component<Props> {
+export default class App extends Component {
   render() {
     return (
       <View style={styles.container}>
